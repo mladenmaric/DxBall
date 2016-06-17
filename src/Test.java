b8c4d180aaeb1470eb8fd879f3ac876c650214c5
lazarvasovic <lzarvas@gmail.com>
2016-06-17 16:22:40 +0200

    konacno2

--- src/Test.java
+++ src/Test.java
@@ -4,11 +4,8 @@ public class Test
 	
 	public static void main(String[] args)
 	{
-<<<<<<< HEAD
 		System.out.println("Let's start :)");		
-=======
-		System.out.println("Let's start :)");
->>>>>>> c8cf43052741f8bcd8f45965e23063801d55ead7
+
 	}
 	
 }
