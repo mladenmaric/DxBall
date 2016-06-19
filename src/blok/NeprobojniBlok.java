1f841970b781e7a9312bc1c202a52374326fcf47
mladenmaric <mladen.pmf@outlook.com>
2016-06-19 22:57:49 +0200

    spojeno

--- src/blok/NeprobojniBlok.java
+++ src/blok/NeprobojniBlok.java
@@ -6,14 +6,17 @@ public class NeprobojniBlok extends Blok
 	public NeprobojniBlok(int x, int y, int sirina, int visina, BojaBloka boja, String pocetnaSlika)
 	{
 		super(x, y, sirina, visina, boja, pocetnaSlika);
-		// TODO Auto-generated constructor stub
 	}
 
-	@Override
 	public void unistiSe(int i, int j, Blok[][] blokovi)
 	{
-		// TODO Auto-generated method stub
-
+		blokovi[i][j].setUnisten(true);
+	}
+	
+	public void unistiSe(int i, int j, Blok[][] blokovi, boolean unisti)
+	{
+		if (unisti)
+			unistiSe(i, j, blokovi);
 	}
 
 }
