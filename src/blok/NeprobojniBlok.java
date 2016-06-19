d979e1e089c9370efde64329adab43a5044b6253
mladenmaric <mladen.pmf@outlook.com>
2016-06-19 22:18:48 +0200

    Radim blokove

---
+++ src/blok/NeprobojniBlok.java
@@ -0,0 +1,19 @@
+package blok;
+
+public class NeprobojniBlok extends Blok
+{
+
+	public NeprobojniBlok(int x, int y, int sirina, int visina, BojaBloka boja, String pocetnaSlika)
+	{
+		super(x, y, sirina, visina, boja, pocetnaSlika);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void unistiSe(int i, int j, Blok[][] blokovi)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
